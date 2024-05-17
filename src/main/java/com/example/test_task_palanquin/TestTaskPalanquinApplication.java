7e47176892a38c740bfbd755ce5e2908b0dda1ce
KofukiTenzo <miranetv12@gmail.com>
2024-05-17 22:17:01 +0300

    first commit

---
+++ src/main/java/com/example/test_task_palanquin/TestTaskPalanquinApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_task_palanquin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskPalanquinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskPalanquinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_task_palanquin/TestTaskPalanquinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_task_palanquin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskPalanquinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
